be15b4d2c068f90ea10f1b2e44ad43cedcfcc7e3
shakur-farit <faritshakur@gmail.com>
2024-10-06 00:47:09 +0400

    created room for lvl 2

--- Assets/Scripts/Tilemap/TilemapLoader.cs
+++ Assets/Scripts/Tilemap/TilemapLoader.cs
@@ -25,6 +25,8 @@ namespace LevelLogic
 
 		private void LoadTilemapData()
 		{
+			Debug.Log($"Load map form {_progressService.Progress.LevelData.RoomData}");
+
 			RoomData roomData = _progressService.Progress.LevelData.RoomData;
 			
 			if(roomData == null)
